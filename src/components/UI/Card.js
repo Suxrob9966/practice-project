d327e909ff42ed8e4b18e3c9ccb26786f3eec95b
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-13 13:42:44 -0600

    build reusable card

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./Card.module.css";
 
 const Card = (props) => {
-  return <div className={styles.card}>{props.children}</div>;
+  return (
+    <div className={`${styles.card} ${props.myOwnClassName}`}>
+      {props.children}
+    </div>
+  );
 };
 
 export default Card;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,26 +1,18 @@
 import React from "react";
 import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
-  const goalInputChangeHandler = (event) => {
-    console.log(event.target.value);
-  };
-
   return (
-    <Card>
+    <Card myOwnClassName={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={goalInputChangeHandler} />
+        <input id="username" type="text" />
         <label htmlFor="age">User Age</label>
-        <input
-          id="age"
-          type="number"
-          min="1"
-          onChange={goalInputChangeHandler}
-        />
+        <input id="age" type="number" min="1" />
         <button type="submit">Add User</button>
       </form>
     </Card>
