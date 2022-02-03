ee83fc39d1079b7055f016528c64872fc5f208e9
Suxrob9966 <rabbimovich9966@gmail.com>
2022-02-03 09:59:55 -0600

    creating a wrapper component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,36 +1,37 @@
-import React, { useState } from "react";
-import Card from "../UI/Card";
-import Button from "../UI/Button";
-import ErrorModal from "../UI/ErrorModal";
-import styles from "./AddUser.module.css";
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
+import styles from './AddUser.module.css';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
-        title: "Invalid input",
-        message: "Please enter a valid name and age (non-empty values)",
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age (non-empty values)',
       });
       return;
     }
 
     if (+enteredAge < 1) {
       setError({
-        title: "Invalid age",
-        message: "Please enter a valid age ( > 0)",
+        title: 'Invalid age',
+        message: 'Please enter a valid age ( > 0)',
       });
       return;
     }
 
     props.onAddUser(enteredUsername, enteredAge);
     // console.log(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -45,7 +46,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -72,7 +73,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
